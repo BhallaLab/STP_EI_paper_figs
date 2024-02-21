e75595cd6f205960c6603a229f3a639359b29dc7
bhalla <bhalla@ncbs.res.in>
2024-02-21 09:27:03 +0530

    cleanup on plotFig9_surprise

--- FIG9_escape/plotFig9_surprise.py
+++ FIG9_escape/plotFig9_surprise.py
@@ -50,6 +50,7 @@ SWEEP = 16
 EPSPTHRESH = 0.0004 # Threshold for a distinct EPSP pk. In Volts
 ALPHAWINDOW = int( 0.32 * SAMPLE_FREQ )
 MINIMUM_PULSE_THRESH = 4e-4
+PulseTrain = {}
 
 
 def alphaFunc( t, tp ):
@@ -63,25 +64,16 @@ def dualAlphaFunc( t, t1, t2 ):
     return (1.0/(t1-t2)) * (np.exp(-t/t1) - np.exp(-t/t2))
 
 
-def findPeaks( pkDelay, Vm, PulseTrain, width = 0.004 ):
+def findPeaks( pkDelay, Vm, pulses, width = 0.004 ):
+    if len( Vm ) < NUM_SAMPLES or len( pulses ) != 33:
+        return []
     widthSamples = int( np.round( width * SAMPLE_FREQ ) )
     half = widthSamples // 2
     pks = []
-    for idx in PulseTrain:
+    for idx in pulses:
         idx2 = idx + pkDelay - widthSamples
-        print( idx, idx2 )
+        #print( idx, idx2, len( Vm ) )
         pks.append(np.quantile( Vm[idx2:idx2 + widthSamples*2], 0.90 ))
-    return pks
-
-def findMaxima( pkDelay, Vm, width = 0.002 ):
-    widthSamples = int( np.round( width * SAMPLE_FREQ ) )
-    half = widthSamples // 2
-    lastPP = 0.0
-    pks = []
-    for idx in PulseTrain:
-        idx2 = idx + pkDelay - widthSamples
-        pks.append(max( Vm[idx2:idx2 + widthSamples*2] ))
-
     return pks
 
 def medianWindowFilter(data):
@@ -115,32 +107,30 @@ def fftFilter( data ):
     '''
     return np.array(data - filtered_data)
 
-def parseRow( df, cell ):
+def parseRow( df, cell, ff ):
+    global PulseTrain
     # Finds the field and epsp peaks for each pulse.
     # If any are too small, it puts in a zero.
     alphaTab, alphaDelay, pkDelay, alphaTau1, alphaTau2 = setFittingParams( cell )
     longAlpha = np.zeros(NUM_SAMPLES)
     longAlpha[:ALPHAWINDOW] += alphaTab
-    epsp = -np.array(df.iloc[0, SAMPLE_START:SAMPLE_START+NUM_SAMPLES ])
+    epsp = np.array(df.iloc[0, SAMPLE_START:SAMPLE_START+NUM_SAMPLES ])
+    pulseTrig = np.array(df.iloc[0, SAMPLE_START+2*NUM_SAMPLES:SAMPLE_START+3*NUM_SAMPLES ] )
     baseline = min( np.percentile(epsp, 25 ), 0.0 )
     epsp -= baseline # hack to handle traces with large ipsps.
     tepsp = np.linspace( 0, 11, len(epsp) )
-    pks = findPeaks( pkDelay, epsp )
-
-    if cell == 111:
-        field = np.array(df.iloc[0, SAMPLE_START + 3*NUM_SAMPLES:SAMPLE_START+4*NUM_SAMPLES ] )
-        field = fftFilter( field )
-        #print( "FIELD: ", np.mean( field ), min( field ), max( field ) )
-        fpks = findMaxima( 100, field, width = 0.002 )
-    else:
-        field = np.zeros( len( epsp ) )
-        fpks = np.zeros(len( PulseTrain ))
+    padt = np.pad( pulseTrig, 1 )
+    edges = ((padt[:-1]< 0.02) & (padt[1:]>0.02) )
+    if sum( edges ) != 33:
+        return []
+    PulseTrain[ff] = np.arange( 0, NUM_SAMPLES, 1, dtype = int )[edges[:NUM_SAMPLES]]
+    pks = findPeaks( pkDelay, epsp, PulseTrain[ff] )
 
     fitEPSP = np.zeros( len( epsp ) + ALPHAWINDOW * 2 )
     lastPP = 0.0
     lastIdx = 0
     epks = []
-    for idx, pp in zip( PulseTrain, pks ):
+    for idx, pp in zip( PulseTrain[ff], pks ):
         ii = idx + alphaDelay
         ascale = pp - lastPP*longAlpha[int(alphaTau1*2) + idx-lastIdx]
         if ascale > 0:
@@ -151,17 +141,7 @@ def parseRow( df, cell ):
         else:
             epks.append( 0.0 )
 
-    if doFieldFitPlot:
-        plt.figure( figsize = (30, 5 ))
-        plt.plot( tepsp, field )
-        print( "LENS = ", len( fpks ), len(PulseTrain), np.median( field ) )
-        plt.scatter( np.array(PulseTrain)/SAMPLE_FREQ, 
-                -np.array(fpks) -np.median(field), c = "red", s = 20 )
-        plt.show()
-
-    assert( len( fpks ) == len( epks ) )
-    assert( len( fpks ) == len( PulseTrain ) )
-    return fpks, epks 
+    return epks 
 
 def panelB_fepspVsTime( ax, fpk5, fpk15 ):
     all5 = np.array( [ ff for ff, ee in fpk5 ] )
@@ -182,18 +162,32 @@ def panelB_fepspVsTime( ax, fpk5, fpk15 ):
     ax.text( -0.20, 1.10, "B", fontsize = 22, weight = "bold", transform=ax.transAxes )
 
 
-def panelC_epspVsTime( ax, pk5, pk15 ):
+def panelC_epspVsTime( ax, pk5, ff, idx ):
     pk5 = np.array( pk5 )
-    pk15 = np.array( pk15 )
+    pk5[pk5>7] = np.median( pk5 )   # filter out outliers.
     mean5 = np.mean( pk5, axis = 0 )
-    mean15 = np.mean( pk15, axis = 0 )
-    ax.scatter( PulseTrain / SAMPLE_FREQ, mean5, color="blue", s=10, label = "5 Sq" )
-    ax.scatter( PulseTrain / SAMPLE_FREQ, mean15, color="orange", s=10, label = "15 Sq" )
+    med5 = np.median( pk5, axis = 0 )
+    t0 = PulseTrain[ff][:9]/SAMPLE_FREQ
+    t1 = PulseTrain[ff][9:17]/SAMPLE_FREQ
+    t2 = PulseTrain[ff][17:25]/SAMPLE_FREQ
+    t3 = PulseTrain[ff][25:]/SAMPLE_FREQ
+    wid = 0.5/ff
+    for pp in pk5:
+        ax.scatter( t0 + np.random.rand( len( t0 ) ) * wid, pp[:9], color="cyan", s=1, label = "5 Sq" )
+        ax.scatter( t1 + np.random.rand( len( t1 ) ) * wid, pp[9:17], color="yellow", s=1, label = "5 Sq" )
+        ax.scatter( t2 + np.random.rand( len( t2 ) ) * wid, pp[17:25], color="pink", s=1, label = "5 Sq" )
+        ax.scatter( t3 + np.random.rand( len( t3 ) ) * wid, pp[25:], color="palegreen", s=1, label = "5 Sq" )
+    #ax.scatter( PulseTrain[ff / SAMPLE_FREQ, mean15, color="orange", s=10, label = "15 Sq" )
+    #ax.scatter( PulseTrain[ff] / SAMPLE_FREQ, mean5, color="blue", s=10, label = "Means" )
+    ax.plot( PulseTrain[ff] / SAMPLE_FREQ, mean5, color="blue", markersize=10, label = "Mean" )
+    ax.plot( PulseTrain[ff] / SAMPLE_FREQ, med5, color="red", markersize=10, label = "Median" )
     ax.spines['top'].set_visible(False)
     ax.spines['right'].set_visible(False)
     ax.set_xlabel( "Time (s)" )
-    ax.set_ylabel( "EPSC (mV )" )
-    ax.text( -0.20, 1.10, "C", fontsize = 22, weight = "bold", transform=ax.transAxes )
+    ax.set_ylabel( "EPSC ratio" )
+    ax.set_ylim( 0, 2.5 )
+    label = chr( ord("B")+idx )
+    ax.text( -0.20, 1.10, label, fontsize = 22, weight = "bold", transform=ax.transAxes )
 
 def panelD_fepspProbVsTime( ax, fpk5, fpk15 ):
     all5 = np.array( [ ff for ff, ee in fpk5 ] )
@@ -348,20 +342,20 @@ def panelA_SampleTrace( ax, dcell ):
     field = np.array(df.iloc[0, SAMPLE_START + 3*NUM_SAMPLES:SAMPLE_START+4*NUM_SAMPLES ] )
     padt = np.pad( pulseTrig, 1 )
     edges = ((padt[:-1]< 0.02) & (padt[1:]>0.02) )
-    PulseTrain = np.arange( 0, NUM_SAMPLES, 1, dtype = int )[edges[:NUM_SAMPLES]]
-    print( PulseTrain )
+    pulses = np.arange(0, NUM_SAMPLES, 1, dtype = int )[edges[:NUM_SAMPLES]]
+    #print( PulseTrain )
 
     baseline = min( np.percentile(epsp, 25 ), 0.0 )
     epsp -= baseline # hack to handle traces with large ipsps.
     tepsp = np.linspace( 0, SAMPLE_TIME, len(epsp) )
-    pks = findPeaks( pkDelay, epsp, PulseTrain )
+    pks = findPeaks( pkDelay, epsp, pulses )
     #print( "NUMPKS = ", len( pks ) )
     fitepsp = np.zeros( len( epsp ) + ALPHAWINDOW * 2 )
     lastPP = 0.0
     lastIdx = 0
     foundPks = []
     foundIdx = []
-    for idx, pp in zip( PulseTrain, pks ):
+    for idx, pp in zip( pulses, pks ):
         ii = idx + alphaDelay
         ascale = pp - lastPP*longAlpha[int(alphaTau1*2) + idx-lastIdx]
         if ascale > 0:
@@ -396,47 +390,35 @@ def scanData( df ):
     idx = 0
     cellStats = {}
     cellList = df['cellID'].unique()
+    temp = df['stimFreq'].unique()
+    freq5 = { ff:[] for ff in temp }
     pk5 = []
-    pk15 = []
-    fpk5 = []
-    fpk15 = []
-    patDict = { pp:[] for pp in [46,47,48,49,50,52,53,55] } # 8 patterns.
     for cellIdx, cell in enumerate( cellList ):
         alphaTab, alphaDelay, pkDelay, alphaTau1, alphaTau2 = setFittingParams( cell )
         dcell = df.loc[df["cellID"] == cell]
-        ipat = dcell["patternList"].astype(int)
-        patList = ipat.unique()
-        for pp in patList:
-            dpat = dcell.loc[ipat == pp]
-            sweepList = dpat['sweep'].unique()
+        freqList = dcell['stimFreq'].unique()
+        for ff in freqList:
+            dfreq = dcell.loc[dcell["stimFreq"] == ff]
+            sweepList = dfreq['sweep'].unique()
             for ss in sweepList:
-                dsweep = dpat.loc[ dpat['sweep'] == ss ]
+                dsweep = dfreq.loc[ dfreq['sweep'] == ss ]
                 seqList = dsweep['exptSeq'].unique()
+                #for seq in [seqList[0],]:
                 for seq in seqList:
                     dseq = dsweep.loc[dsweep['exptSeq'] == seq]
-                    print( "{}, cell{}, pat{}, sweep{}, seq{}".format( 
-                        idx, cell, pp, ss, seq ) )
                     idx += 1
-                    fpks, epks = parseRow( dseq, cell )
-                    #print( "SUM = ", sum( foundIdx ) )
-                    patDict[pp].append( [fpks, epks] )
-                    if pp in [46,47,48,49,50]:
-                        pk5.append(epks)
-                        '''
-                        if cell == 4041: # The only one with field data
-                            print( "Appending 4041 ", len( fpks ), len( epks ) )
-                            fpk5.append( [fpks, epks] )
-                        '''
-                        fpk5.append( [fpks, epks] )
-                    else:
-                        pk15.append(epks)
-                        fpk15.append( [fpks, epks] )
-                        '''
-                        if cell == 4041: # The only one with field data
-                            fpk15.append( [fpks, epks] )
-                        '''
-    
-    return pk5, pk15, fpk5, fpk15, patDict
+                    epks = parseRow( dseq, cell, ff )
+                    OK = (len( epks ) > 0 and epks[0] > np.mean( epks )*0.2)
+                    print( "{}, cell{}, freq{}, sweep{}, seq{}, {}".format( 
+                        idx, cell, ff, ss, seq, "" if OK else "bad" ) )
+                    if OK:
+                        norm = np.array(epks)/epks[0]
+                        pk5.append( norm )
+                        freq5[ff].append( norm )
+                        #if np.mean( epks )> 10:
+                        #    print( "MEAN = ", np.mean(epks) )
+
+    return pk5, freq5
 
 def panelLM_varianceHisto( ax1, ax2, patDict ):
     totfvar = []
@@ -515,6 +497,10 @@ def main():
     # Set up the stimulus timings
     df = pandas.read_hdf( patternData )
     cells = df['cellID'].unique()
+    #pats = df['patternList']
+    #pats = "    PATS   "
+    #print( "Uniques", df['probePulseStart'].unique(), df['sweepLength'].unique(), cells, pats )
+    #print( [len( foo ) for foo in df['patternList']] )
     '''
     for cc in cells:
         cdf = df.loc[df['cellID']== cc]
@@ -522,22 +508,25 @@ def main():
     print( "NUM COLS = ",len( df.columns ) )
     '''
     ax = fig.add_subplot( gs[0,:] )
+    #celldf = df.loc[df['cellID'] == 2822]
+    #celldf = df.loc[df['cellID'] == 2822]
+    #celldf = df.loc[df['cellID'] == 2681]
     panelA_SampleTrace( ax, df )
-    plt.show()
-    pk5, pk15, fpk5, fpk15, patDict = scanData( df )
-    print( len( pk5 ), len( pk15 ), len( fpk5 ), len( fpk15 ) )
-    print( len( pk5[0] ), len( pk15[0] ), len( fpk5[0] ), len( fpk15[0] ) )
-    panelB_fepspVsTime( fig.add_subplot(gs[1,0]), fpk5, fpk15 )
-    panelC_epspVsTime( fig.add_subplot(gs[1,1]), pk5, pk15 )
-    panelD_fepspProbVsTime( fig.add_subplot(gs[2,0]), fpk5, fpk15 )
-    panelE_epspProbVsTime( fig.add_subplot(gs[2,1]), pk5, pk15 )
-    panelF_fepspVsISI(  fig.add_subplot(gs[3,0]), fpk5, fpk15 )
-    panelG_epspVsISI(  fig.add_subplot(gs[3,1]), pk5, pk15 )
-    panelHI_epspVsField( fig.add_subplot(gs[4,0]), fpk5, "H" )
-    panelHI_epspVsField( fig.add_subplot(gs[4,1]), fpk15, "I" )
-    panelJ_fpkHisto( fig.add_subplot(gs[5,0]), fpk5, fpk15 )
-    panelK_epkHisto( fig.add_subplot(gs[5,1]), pk5, pk15 )
-    panelLM_varianceHisto( fig.add_subplot(gs[6,0]), fig.add_subplot(gs[6,1]), patDict )
+    pk5, freq5 = scanData( df )
+    print( len( pk5 ) )
+    #print( len( pk5[0] ) )
+    #panelB_fepspVsTime( fig.add_subplot(gs[1,0]), fpk5, fpk15 )
+    for idx, ff in enumerate(freq5):
+        panelC_epspVsTime( fig.add_subplot(gs[idx+1,:]), freq5[ff], ff, idx)
+    #panelD_fepspProbVsTime( fig.add_subplot(gs[2,0]), fpk5, fpk15 )
+    #panelE_epspProbVsTime( fig.add_subplot(gs[2,1]), pk5, pk5 )
+    #panelF_fepspVsISI(  fig.add_subplot(gs[3,0]), fpk5, fpk15 )
+    #panelG_epspVsISI(  fig.add_subplot(gs[3,1]), pk5, pk5 )
+    #panelHI_epspVsField( fig.add_subplot(gs[4,0]), fpk5, "H" )
+    #panelHI_epspVsField( fig.add_subplot(gs[4,1]), fpk15, "I" )
+    #panelJ_fpkHisto( fig.add_subplot(gs[5,0]), fpk5, fpk15 )
+    #panelK_epkHisto( fig.add_subplot(gs[5,1]), pk5, pk5 )
+    #panelLM_varianceHisto( fig.add_subplot(gs[6,0]), fig.add_subplot(gs[6,1]), patDict )
     
     fig.tight_layout()
     plt.show()
