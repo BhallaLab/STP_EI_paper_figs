8495e1f71e2bb37dcd61f129193bb46b56465d4b
bhalla <bhalla@ncbs.res.in>
2024-09-02 15:51:29 +0530

    Updated plotFig3 to use data from VCL16

--- FIG3_Presyn_model/plotFig3.py
+++ FIG3_Presyn_model/plotFig3.py
@@ -1,49 +1,44 @@
+##
+## plotFig3.py
+## This file generates Figure 3 panels B to G.
+##
+## It is licenced under GPLv3.0 or later
+## Copyright (c) Upinder S. Bhalla
+##
+
 import os
 import findSim
 import numpy as np
 import pandas
 import matplotlib.pyplot as plt
 
-
-# Get rid of the frames
-# Get rid of time axis except for bottom plot.
-# Convert solid blue line into just the dots. Better yet, 
-# link above and below
 def doPlot( dataset, idx, panel, freq, doProb = False ):
     cell = dataset.split("_")[0]
     exc = "Exc" if dataset.split("_")[1] == "1" else "Inh"
-    if not os.path.isfile("ExptFilesForFigure/fs_{0}_{1}_{1}_pk.json".format(dataset,freq)):
-        print("Not found: ExptFilesForFigure/fs_{0}_{1}_{1}_pk.json".format(dataset,freq))
+    if not os.path.isfile("Expts/fs_{0}_{1}_pk.json".format(dataset,freq)):
+        print("Not found: Expts/fs_{0}_{1}_pk.json".format(dataset,freq))
         return
     ax = plt.subplot( 3, 2, idx )
     ax.text( -0.10, 1.05, panel, fontsize = 22, weight = "bold", 
         transform=ax.transAxes )
     if doProb:
         score, elapsedTime, diagnostics = findSim.innerMain( 
-            #"ExptFilesForFigure/fs_7492_1_5_49_{}_pk.json".format( freq ), 
-            #modelFile = "ResultsForFigure/opt7492_1_5_49.g", 
-            "ExptFilesForFigure/fs_{}_{}_prob.json".format( dataset, freq ),
+            "Expts/fs_{}_prob.json".format( dataset ),
             modelFile = "Models/{}_vclamp{}.py".format( exc, cell ), 
-            chemFile = "Models/BothPresyn77.g",
+            chemFile = "Models/BothPresyn86.g",
             mapFile = "Maps/mapPresyn{}.json".format( exc ),
             bigFont = True, labelPos = "upper left", deferPlot = True )
         ex = np.array( diagnostics["exptX"] )
         ey = np.array( diagnostics["exptY"] )
         ax.plot( ex, ey, "bo-" )
-        #upx = ex[::2]
-        #dnx = ex[1::2]
-        #upy = ey[::2]
-        #dny = ey[1::2]
     else:
         score, elapsedTime, diagnostics = findSim.innerMain( 
-            #"ExptFilesForFigure/fs_7492_1_5_49_{}_pk.json".format( freq ), 
-            #modelFile = "ResultsForFigure/opt7492_1_5_49.g", 
-            "ExptFilesForFigure/fs_{0}_{1}_{1}_pk.json".format( dataset, freq), 
+            "Expts/fs_{0}_{1}_pk.json".format( dataset, freq), 
             modelFile = "Models/{}_vclamp{}.py".format( exc, cell ), 
-            chemFile = "Models/BothPresyn77.g", 
+            chemFile = "Models/BothPresyn86.g", 
             mapFile = "Maps/mapPresyn{}.json".format( exc ),
             bigFont = True, labelPos = "upper left", deferPlot = True )
-        ex = np.array( diagnostics["exptX"] )
+        ex = np.array( diagnostics["exptX"] ) - 0.008
         ey = np.array( diagnostics["exptY"] )
         upx = ex[::2]
         dnx = ex[1::2]
@@ -51,24 +46,20 @@ def doPlot( dataset, idx, panel, freq, doProb = False ):
         dny = ey[1::2]
         ax.plot( upx, upy, "bo-" )
         ax.plot( dnx, dny, "bo-" )
-    #ax.lines[3].set_visible( False )
     ax.spines['top'].set_visible(False)
     ax.spines['right'].set_visible(False)
     ax.lines[2].set_visible( False )
     ax.lines[1].set_visible( False )
-    #ax.lines[0].set_visible( False )
     ax.lines[3].set_visible( False )
-    #ax.lines[1].set_linestyle( None )
     if doProb:
         ax.set_xlim( 0.95, 1.05 )
         #ax.set_ylim( -1, 41 )
     else:
         ax.set_xlim( 0.95, 1.7 )
-        ax.set_ylim( -0.05, 1.6 )
+        ax.set_ylim( -0.05, 1.5 )
     plt.title("")
 
 def doScoreHisto( ax ):
-    #bins = "auto"
     bins = np.array( np.arange( 0, 0.7501, 0.05 ) )
     df = pandas.read_hdf( "model_params.h5" )
     #df.columns == ['cell', 'exc', 'numSq', 'pattern', 'initScore', 
@@ -93,27 +84,20 @@ def doScoreHisto( ax ):
 
 
 def main():
-    #for cell in ["7492", "7491", "1621"]:
-    #for cell in ["1931", "1524", "1522", "1621", "111", "7491"]:
-    for cell in ["7492", "1491", "6301", "6201", "1541", "1531"]:
-    #for cell in ["1621"]:
-        #for exc in ["0", "1"]:
-        for exc in ["1",]:
-            #for pat in [ "5_46", "5_47", "5_48", "5_49", "5_50", "15_52", "15_53", "15_55"]: 
-            for pat in [ "5"]: 
-
-                dataset = "{}_{}_{}".format( cell, exc, pat )
-                fig = plt.figure( figsize = (10, 12), facecolor = "white" )
-                fig.suptitle( dataset )
-                doPlot( dataset, 1, "B", 20 )
-                if cell not in ["7491", "1931"]:
-                    doPlot( dataset, 2, "C", 30 )
-                    doPlot( dataset, 3, "D", 40 )
-                doPlot( dataset, 4, "E", 50 )
-                doPlot( dataset, 5, "F", 50, doProb = True )
-                ax = plt.subplot( 3,2,6 )
-                doScoreHisto(ax)
-                plt.show()
+    cell = "7492"
+    exc = "1"
+    pat = "5"
+    dataset = "{}_{}_{}".format( cell, exc, pat )
+    fig = plt.figure( figsize = (10, 12), facecolor = "white" )
+    fig.suptitle( dataset )
+    doPlot( dataset, 1, "B", 20 )
+    doPlot( dataset, 2, "C", 30 )
+    doPlot( dataset, 3, "D", 40 )
+    doPlot( dataset, 4, "E", 50 )
+    doPlot( dataset, 5, "F", 50, doProb = True )
+    ax = plt.subplot( 3,2,6 )
+    doScoreHisto(ax)
+    plt.show()
 
     plt.tight_layout()
     plt.show()
